3b872782dfb3b02c2777c1d38c0ae6b231975785
Armaan Priadarshan <43357808+armaanpriyadarshan@users.noreply.github.com>
2020-08-10 17:54:24 -0400

    Update TF-image-od.py

--- workspace/training_demo/TF-image-od.py
+++ workspace/training_demo/TF-image-od.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 """
-Object Detection From TF2 Saved Model
+Object Detection (On Image) From TF2 Saved Model
 =====================================
 """
 
-# %%
-# This demo will take you through the steps of running an "out-of-the-box" TensorFlow 2 compatible
-# detection model on a collection of images. More specifically, in this example we will be using
-# the `Saved Model Format <https://www.tensorflow.org/guide/saved_model>`__ to load the model.
-
-# %%
-# Download the test images
-# ~~~~~~~~~~~~~~~~~~~~~~~~
-# First we will download the images that we will use throughout this tutorial. The code snippet
-# shown bellow will download the test images from the `TensorFlow Model Garden <https://github.com/tensorflow/models/tree/master/research/object_detection/test_images>`_
-# and save them inside the ``data/images`` folder.
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging (1)
 import pathlib
@@ -29,48 +18,20 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
+# PROVIDE PATH TO IMAGE DIRECTORY
 IMAGE_PATHS = "images/test/i-1e092ec6eabf47f9b85795a9e069181b.jpg"
 
 
-# %%
-# Download the model
-# ~~~~~~~~~~~~~~~~~~
-# The code snippet shown below is used to download the pre-trained object detection model we shall
-# use to perform inference. The particular detection algorithm we will use is the
-# `CenterNet HourGlass104 1024x1024`. More models can be found in the `TensorFlow 2 Detection Model Zoo <https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf2_detection_zoo.md>`_.
-# To use a different model you will need the URL name of the specific model. This can be done as
-# follows:
-#
-# 1. Right click on the `Model name` of the model you would like to use;
-# 2. Click on `Copy link address` to copy the download link of the model;
-# 3. Paste the link in a text editor of your choice. You should observe a link similar to ``download.tensorflow.org/models/object_detection/tf2/YYYYYYYY/XXXXXXXXX.tar.gz``;
-# 4. Copy the ``XXXXXXXXX`` part of the link and use it to replace the value of the ``MODEL_NAME`` variable in the code shown below;
-# 5. Copy the ``YYYYYYYY`` part of the link and use it to replace the value of the ``MODEL_DATE`` variable in the code shown below.
-#
-# For example, the download link for the model used below is: ``download.tensorflow.org/models/object_detection/tf2/20200711/centernet_hg104_1024x1024_coco17_tpu-32.tar.gz``
-
-# Download and extract model
-
+# PROVIDE PATH TO MODEL DIRECTORY
 PATH_TO_MODEL_DIR = "exported-models/my_mobilenet_model"
 
-# %%
-# Download the labels
-# ~~~~~~~~~~~~~~~~~~~
-# The coode snippet shown below is used to download the labels file (.pbtxt) which contains a list
-# of strings used to add the correct label to each detection (e.g. person). Since the pre-trained
-# model we will use has been trained on the COCO dataset, we will need to download the labels file
-# corresponding to this dataset, named ``mscoco_label_map.pbtxt``. A full list of the labels files
-# included in the TensorFlow Models Garden can be found `here <https://github.com/tensorflow/models/tree/master/research/object_detection/data>`__.
-
-# Download labels file
+# PROVIDE PATH TO LABEL MAP
 
 
 PATH_TO_LABELS = "exported-models/my_mobilenet_model/saved_model/label_map.pbtxt"
 
-# %%
-# Load the model
-# ~~~~~~~~~~~~~~
-# Next we load the downloaded model
+# LOAD THE MODEL
+
 import time
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as viz_utils
@@ -80,39 +41,18 @@ PATH_TO_SAVED_MODEL = PATH_TO_MODEL_DIR + "/saved_model"
 print('Loading model...', end='')
 start_time = time.time()
 
-# Load saved model and build the detection function
+# LOAD SAVED MODEL AND BUILD DETECTION FUNCTION
 detect_fn = tf.saved_model.load(PATH_TO_SAVED_MODEL)
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 print('Done! Took {} seconds'.format(elapsed_time))
 
-# %%
-# Load label map data (for plotting)
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Label maps correspond index numbers to category names, so that when our convolution network
-# predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility
-# functions, but anything that returns a dictionary mapping integers to appropriate string labels
-# would be fine.
+# LOAD LABEL MAP DATA FOR PLOTTING
 
 category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
                                                                     use_display_name=True)
 
-# %%
-# Putting everything together
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# The code shown below loads an image, runs it through the detection model and visualizes the
-# detection results, including the keypoints.
-#
-# Note that this will take a long time (several minutes) the first time you run this code due to
-# tf.function's trace-compilation --- on subsequent runs (e.g. on new images), things will be
-# faster.
-#
-# Here are some simple things to try out if you are curious:
-#
-# * Modify some of the input images and see if detection still works. Some simple things to try out here (just uncomment the relevant portions of code) include flipping the image horizontally, or converting to grayscale (note that we still expect the input image to have 3 channels).
-# * Print out `detections['detection_boxes']` and try to match the box locations to the boxes in the image.  Notice that coordinates are given in normalized form (i.e., in the interval [0, 1]).
-# * Set ``min_score_thresh`` to other values (between 0 and 1) to allow more detections in or to filter out more detections.
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -142,13 +82,6 @@ print('Running inference for {}... '.format(IMAGE_PATHS), end='')
 image = cv2.imread(IMAGE_PATHS)
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 image_expanded = np.expand_dims(image_rgb, axis=0)
-# Things to try:
-# Flip horizontally
-# image_np = np.fliplr(image_np).copy()
-
-# Convert image to grayscale
-# image_np = np.tile(
-#     np.mean(image_np, 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
 
 # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
 input_tensor = tf.convert_to_tensor(image)
@@ -171,6 +104,7 @@ detections['detection_classes'] = detections['detection_classes'].astype(np.int6
 
 image_with_detections = image.copy()
 
+# SET MIN_SCORE_THRESH BASED ON YOU MINIMUM THRESHOLD FOR DETECTIONS
 viz_utils.visualize_boxes_and_labels_on_image_array(
       image_with_detections,
       detections['detection_boxes'],
@@ -179,7 +113,7 @@ viz_utils.visualize_boxes_and_labels_on_image_array(
       category_index,
       use_normalized_coordinates=True,
       max_boxes_to_draw=200,
-      min_score_thresh=.30,
+      min_score_thresh=.60,
       agnostic_mode=False)
 
 print('Done')
@@ -188,4 +122,4 @@ cv2.imshow('Object Detector', image_with_detections)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
-# sphinx_gallery_thumbnail_number = 2
+
