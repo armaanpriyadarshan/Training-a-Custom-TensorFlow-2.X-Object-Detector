b835902cc1e829f433b878fdd0c37fe74d3bf6ab
Armaan Priadarshan <43357808+armaanpriyadarshan@users.noreply.github.com>
2020-08-24 12:46:03 -0400

    Update convert-to-tflite.py

--- workspace/training_demo/convert-to-tflite.py
+++ workspace/training_demo/convert-to-tflite.py
@@ -7,6 +7,7 @@ root.v2 = tf.Variable(2.)
 root.f = tf.function(lambda x: root.v1 * root.v2 * x)
 
 # Save the model in SavedModel format.
+# Provide your SavedModel Directory in export_dir
 export_dir = "exported-models/my_mobilenet_model/saved_model"
 input_data = tf.constant(1., shape=[1, 1])
 to_save = root.f.get_concrete_function(input_data)
