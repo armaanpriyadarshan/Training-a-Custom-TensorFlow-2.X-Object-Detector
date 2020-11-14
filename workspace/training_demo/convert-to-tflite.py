4ca2cae2d03d6f0ad48cb2a95a64c164b09ba0f3
Armaan Priadarshan <43357808+armaanpriyadarshan@users.noreply.github.com>
2020-11-14 09:16:22 -0500

    Add files via upload

---
+++ workspace/training_demo/convert-to-tflite.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import argparse
+# Define model and output directory arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', help='Folder that the saved model is located in',
+                    default='exported-models/my_tflite_model_3/saved_model')
+parser.add_argument('--output', help='Folder that the tflite model will be written to',
+                    default='exported-models/my_tflite_model_3/saved_model')
+args = parser.parse_args()
+
+converter = tf.lite.TFLiteConverter.from_saved_model(args.model)
+#converter = tf.compat.v1.lite.TFLiteConverter.from_saved_model('exported-models/my_tflite_model/saved_model')
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+#def representative_dataset_gen():
+#  for _ in range(num_calibration_steps):
+#    # Get sample input data as a numpy array in a method of your choosing.
+#    yield [input]
+#converter.representative_dataset = representative_dataset_gen
+converter.experimental_new_converter = True
+#converter.enable_v1_converter = True
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
+
+tflite_model = converter.convert()
+
+output = args.output + '/model.tflite'
+with tf.io.gfile.GFile(output, 'wb') as f:
+  f.write(tflite_model)
